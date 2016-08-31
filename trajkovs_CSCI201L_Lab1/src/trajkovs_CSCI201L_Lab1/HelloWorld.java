1e7f4ef39bf9d86be0f8689a96e42d27badaa470
Nikica Trajkovski <trajkovs@usc.com>
2016-08-31 00:25:48 -0700

    lab

---
+++ trajkovs_CSCI201L_Lab1/src/trajkovs_CSCI201L_Lab1/HelloWorld.java
@@ -0,0 +1,15 @@
+package trajkovs_CSCI201L_Lab1;
+
+import java.util.Scanner;
+public class HelloWorld {
+	public static void main(String [] args) {
+//		System.out.println("Hello World");
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter your name: ");
+		
+		String name = in.nextLine();
+		System.out.println("Hello " + name);
+//		s = null;
+//		s.length();
+	}
+}
